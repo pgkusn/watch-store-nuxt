07f9400591fa6af51670310ef76314f407f791b3
kenge <pgkusn@gmail.com>
2021-08-12 12:08:25 +0800

    修復分頁在重整時出現的 error

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="pages > 1" class="flex justify-center mt-10 md:mt-15 text-xl leading-6 text-coral-black">
+    <div class="flex justify-center mt-10 md:mt-15 text-xl leading-6 text-coral-black" :class="{ hidden: pages < 2 }">
         <nuxt-link
             v-for="n in pages"
             :key="n"
